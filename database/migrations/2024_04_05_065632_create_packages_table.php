1c5efb93ef9d6a9999e3bfc6f9dffc1268b21afe
abhisheksharmashine <abhishek.singh@shinedezign.com>
2024-04-05 17:44:52 +0530

    fix

--- database/migrations/2024_04_05_065632_create_packages_table.php
+++ database/migrations/2024_04_05_065632_create_packages_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name')->nullable();
             $table->longText('description')->nullable();
             $table->float('price')->nullable();
-            $table->enum('billing_cycle', ['Mounthly', 'Every 3 months', 'Every 6 months', 'Yearly'])->default('Mounthly');
+            $table->enum('billing_cycle', ['Monthly', 'Yearly', 'Quarterly', 'Halfly'])->default('Monthly');
             $table->integer('trial_days')->nullable();
             $table->string('stripe_id')->nullable();
             $table->string('stripe_price')->nullable();
