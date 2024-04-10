e93f06fded9e1a7224541cad14578c2805214b03
abhisheksharmashine <abhishek.singh@shinedezign.com>
2024-04-10 13:17:42 +0530

    change

--- resources/views/dealer/sidebar.blade.php
+++ resources/views/dealer/sidebar.blade.php
@@ -31,7 +31,7 @@
                 </div>
             </li>
             <li class="analyics-tabs-list">
-                <div class="accordion accordion-flush" id="accordionFlushExample">
+                {{-- <div class="accordion accordion-flush" id="accordionFlushExample">
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="flush-headingOne">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
@@ -56,7 +56,7 @@
                             </button>
                         </h2>
                     </div>
-                </div>
+                </div> --}}
             </li>
             <li class="analyics-tabs-list">
                 <div class="accordion accordion-flush" id="accordionFlushExample">
