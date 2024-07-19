c65cfb72eca61bbefa4e8fb4677e9dc1d768470a
vikramkamad <vikram.kamad@shinedezign.com>
2024-07-19 05:58:19 +0000

    fixes

---
+++ app/Console/Commands/DeleteDuplicateModels.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\AllModel;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class DeleteDuplicateModels extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'distnict:models';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $idsToKeep = AllModel::select(DB::raw('MIN(id) as id'))
+        ->groupBy('value')
+        ->pluck('id')
+        ->toArray();
+    }
+}
