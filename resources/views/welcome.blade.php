a8f437f1f8f782c843ead913d26ecd4ca39b77b6
sumit-dezigner <sumit.kumar@shinedezign.com>
2024-07-23 17:01:04 +0530

    welcome page view-more slider design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -381,7 +381,46 @@
         prevArrow: $('.prev-btn'),
         nextArrow: $('.next-btn'),
     });
+    $('.categories-loop-boxes').slick({
+        infinite: true,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        prevArrow: $('.prev-loop-btn'),
+        nextArrow: $('.next-loop-btn'),
+        responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              slidesToShow: 5,
+              slidesToScroll: 2,
+            }
+          },
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 2,
+            }
+          },
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+            }
+          },
+          {
+            breakpoint: 425,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+          }
 
+        ]
+    });
 
     $(document).ready(function() {
         $(document).on('click', '.addtocart', function() {
