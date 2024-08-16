31b2c2cb8ce178406ddd0f51ddd59b31fa2e544e
vikramkamad <vikram.kamad@shinedezign.com>
2024-08-16 07:18:24 +0000

    fixing migration table name

--- database/migrations/2024_08_12_072556_add_stripe_account_id_to_user.php
+++ database/migrations/2024_08_12_072556_add_stripe_account_id_to_user.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('Users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
            $table->text('stripe_account_id')->nullable()->after('remember_token');
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('Users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('stripe_account_id');
         });
     }
